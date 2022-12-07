01a5ea7388c8cddf7ef9291bc721cfd246c72cd5
NICKY-TECH <tinaonyis@gmail.com>
2022-12-07 09:56:39 +0100

    project structure

---
+++ backend/Model/userSchema.model.js
@@ -0,0 +1,35 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    firstname:{
+        required: true,
+        type: String
+    },
+    lastname:{
+        required:true,
+        type:String
+    },
+    username:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required: true,
+    },
+    phonenumber:{
+        required: true,
+        type:String
+    }
+
+},{
+    timestamps:true
+});
+
+
+
+const user=mongoose.model('User',userSchema);
+
+module.exports={
+    user
+}
